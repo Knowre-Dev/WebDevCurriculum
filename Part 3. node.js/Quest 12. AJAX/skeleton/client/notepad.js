d8c4f786ff11d7497e5dbea78db48d6b35c709f5
yjpark <yjpark@knowre.com>
2018-08-16 19:17:12 +0900

    doing

--- Part 3. node.js/Quest 12. AJAX/skeleton/client/notepad.js
+++ Part 3. node.js/Quest 12. AJAX/skeleton/client/notepad.js
@@ -1,15 +1,68 @@
 class Notepad {
 	/* TODO: 그 외에 또 어떤 클래스와 메소드가 정의되어야 할까요? */
-	init() {
-
+	constructor(){
+		this._init();
 	}
 
-	_ajax(url, method, data, header, body){
-		fetch(url, {
-			method: method,
-			data: data
-		}).then(res => {
-			return res;
+	_init(){
+		this._fetch_files();
+		this._btn_event();
+	}
+
+	_fetch_files(){
+		fetch('http://localhost:8080/files')
+		.then(res => res.json())
+		.then(result => {
+			result.output.map(element => {
+				let div = document.createElement('div');
+				div.className = 'file';
+				let span = document.createElement('span');
+				span.innerHTML = element;
+				div.appendChild(span);
+				this._get_event(div);
+				
+				document.getElementById('files').appendChild(div);
+			});	
 		});
+	}
+
+	_get_event(div){
+		div.onclick = () => {
+			let filename = div.children[0].innerHTML;
+			fetch('http://localhost:8080/files/' + filename)
+			.then(res => res.json())
+			.then(result => {
+				let textarea = document.getElementById('content_text');
+				textarea.value = result.output;
+				textarea.name = filename;
+				textarea.disabled = false;
+			});
+		};
+	}
+
+	_btn_event(){
+		//this._new_event();
+		this._save_event();
+	}
+
+	_save_event(){
+		let btn_save = document.getElementById('save_btn');
+		btn_save.onclick = () => {
+			let textarea = document.getElementById('content_text');
+			fetch('http://localhost:8080/files/' + textarea.name, {
+				method: 'POST',
+				headers: {
+					'Content-type': 'application/x-www-form-urlencoded; charset=utf-8'
+				},
+				body: textarea.value,
+			})
+			.then(res => res.json())
+			.then(result => {})
+		}
+	}
+
+	_new_event(){
+		let new_btn = document.getElementById('new_btn');
+
 	}
 };
--- Part 3. node.js/Quest 12. AJAX/skeleton/server.js
+++ Part 3. node.js/Quest 12. AJAX/skeleton/server.js
@@ -5,6 +5,8 @@ const express = require('express'),
 
 app.use(express.static('client'));
 
+app.use(express.urlencoded({extended: false}));
+
 app.get('/', (req, res) => {
 	res.sendFile(path.join(__dirname, 'index.html'));
 });
@@ -15,10 +17,31 @@ const server = app.listen(8080, () => {
 
 /* TODO: 여기에 처리해야 할 요청의 주소별로 동작을 채워넣어 보세요..! */
 app.get('/files', (req, res) => {
-
+	let filepath = path.join(__dirname, 'memo');
+	fs.readdir(filepath, (err, data) => {
+		res.send(JSON.stringify({output: data}));
+	});
 });
 
-async function get_files(path){
-	const files = await fs.readdir(path);
-	
-}
+app.get('/files/:filename', (req, res) => {
+	let filename = req.params.filename;
+	let filepath = path.join(__dirname, 'memo', filename);
+	fs.readFile(filepath, 'utf8', (err,data) =>{
+		res.send(JSON.stringify({output: data}));
+	});
+});
+
+app.post('/files/:filename', (req, res) => {
+	let filename = req.params.filename;
+	console.log(req.body);
+	console.log(filename);
+	let filepath = path.join(__dirname, 'memo', filename);
+	// fs.writeFile(filepath, data, 'utf8', (err) =>{
+	// 	console.log('success');
+	// });
+});
+
+app.post('files/newfile', (req, res) => {
+	console.log(req.body);
+});
+
