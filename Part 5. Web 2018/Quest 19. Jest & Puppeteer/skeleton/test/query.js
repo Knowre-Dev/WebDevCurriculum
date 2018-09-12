22b741f5fbbe40474dc1db95f819e725703310f0
yjpark <wayofjin@gmail.com>
2018-09-12 17:08:55 +0900

    quest

--- Part 5. Web 2018/Quest 19. Jest & Puppeteer/skeleton/test/query.js
+++ Part 5. Web 2018/Quest 19. Jest & Puppeteer/skeleton/test/query.js
@@ -1,11 +1,28 @@
 const server = require('../server.js');
 const r2 = require('r2');
 
-test('test', async () => {
-    let res = await r2.post('http://localhost:8080').response;
-    console.log('test done');
-});
 
 afterAll(() => {
     server.close();
+});
+
+const headers = {
+    'Content-Type': 'application/json'
+};
+
+test('test', async () => {
+    let res = await r2.post('http://localhost:8080/graphql', {
+        headers:headers,
+        body: JSON.stringify({
+            query: `
+                query {
+                    getUser(id: "test1"){
+                        id
+                        name
+                    }
+                }
+            `
+        })
+    }).json;
+    console.log(res);
 });
