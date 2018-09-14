5126a009d537dff20d443d86889d43726fe63ffa
yjpark <yjpark@knowre.com>
2018-09-14 14:22:21 +0900

    update

--- Part 5. Web 2018/Quest 19. Jest & Puppeteer/skeleton/test/query.js
+++ Part 5. Web 2018/Quest 19. Jest & Puppeteer/skeleton/test/query.js
@@ -54,5 +54,33 @@ test('get memo by userid and title', async () => {
             `
         })
     }).json;
-    console.log(res);
+    expect(res.data.getMemo.title).toBe('first title');
+});
+
+test('create memo by userid and title', async () => {
+    const res = await r2.post(testUrl, {
+        headers: headers,
+        body: JSON.stringify({
+            query: `
+                mutation {
+                    createMemo(userId: "test1", title: "third title")
+                }
+            `
+        })
+    }).json;
+    expect(res.data.createMemo).toBe(3);
+});
+
+test('update memo by userid and title', async () => {
+    const res = await r2.post(testUrl, {
+        headers: headers,
+        body: JSON.stringify({
+            query: `
+                mutation {
+                    updateMemo(userId: "test1", title: "first title", content: "test", lastPosition: 0)
+                }
+            `
+        })
+    }).json;
+    expect(res.data.updateMemo).toBe(1);
 });
