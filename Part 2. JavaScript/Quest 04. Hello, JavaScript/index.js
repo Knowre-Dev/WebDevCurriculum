47f4ec9ccb8523a5fee7d06c4e26967171681d78
YunMin-Song <thd930308@naver.com>
2021-01-10 18:55:12 +0900

    Part 2 CheckList

--- Part 2. JavaScript/Quest 04. Hello, JavaScript/index.js
+++ Part 2. JavaScript/Quest 04. Hello, JavaScript/index.js
@@ -1,35 +1,19 @@
 function stars(num) {
     let i, j, res;
-    for (i = 0; i < num; i++) {
+    for (i = 0; i < num; i++) {             // n번 반복
         res = "";
-        for(j=0;j<num+(num-1);j++){
-            if(j<=num-1+i && j>= num-1-i){
+        for (j = 0; j < num + (num - 1); j++) {         // n = 4, 반복 당 넓이 : 4+(4-1) = 7
+            if (j <= num - 1 + i && j >= num - 1 - i) {  // 좌우의 Space 구현
                 res += "*";
-            }
-            else{
-                res += "&nbsp";
+            } else {
+                res += " ";
             }
         }
         document.write(res);
-        document.write("<br>");
+        console.log(res);
+        console.log("\n");
     }
 }
 
 let num = prompt();
 stars(num)
-
-
-/* if Unused Code */
-// for (i = 0; i < num; i++) {
-//     for (j = num-1; j > i ; j--) {
-//         console.log(" ");
-//         document.write("&nbsp");
-//     }
-//
-//     for(j=0;j<=i*2;j++){
-//         console.log("*");
-//         document.write("*");
-//     }
-//     document.write("<br>");
-//     console.log("<br>");
-// }
