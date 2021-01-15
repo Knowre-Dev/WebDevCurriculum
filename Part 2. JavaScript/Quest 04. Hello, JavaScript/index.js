bb6558d89c1e1a9dd148201d4bf51bb3ce0d41d4
Song-YunMin <thd930308@naver.com>
2021-01-15 10:02:03 +0900

    [UPDATE] : Quest 4 수정사항 및 Quest 5 추가

--- Part 2. JavaScript/Quest 04. Hello, JavaScript/index.js
+++ Part 2. JavaScript/Quest 04. Hello, JavaScript/index.js
@@ -4,8 +4,7 @@ function stars(num) {
         // num - i + 1 만큼 공백 출력
         // i + 1 만큼 * 출력
         console.log(
-            letters(num - i, " ")
-            +letters(i*2+1,"*")
+            letters(num - i, " ") + letters(i*2+1,"*")
         );
     }
 }
@@ -14,9 +13,11 @@ function stars(num) {
 // Array.join : 구분자를 치환하여 연결 함
 
 function letters(num, letter){
-    let arr = new Array(num);
-    arr.fill(letter);
-    return arr.join("");
+    let arr = new Array(num);       // num 길이의 배열 생성
+    arr.fill(letter);               // 배열을 letter 의 값으로 치환함
+    return arr.join("");            // join 하여 구분자 기준 연결
+
+    // 아래와 같은 방법으로도 가능
     // return (new Array(num)).fill(letter).join("");
 }
 
