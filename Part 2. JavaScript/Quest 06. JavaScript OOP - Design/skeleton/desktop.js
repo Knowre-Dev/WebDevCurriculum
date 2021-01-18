11b285f2ec8f69be6b74b3fb2d4caac00d834e68
Song-YunMin <thd930308@naver.com>
2021-01-18 11:14:06 +0900

    [ETC] : Quest 6 초기 코드 테스트

--- Part 2. JavaScript/Quest 06. JavaScript OOP - Design/skeleton/desktop.js
+++ Part 2. JavaScript/Quest 06. JavaScript OOP - Design/skeleton/desktop.js
@@ -1,15 +1,42 @@
+// 바탕 화면
+// 처음에는 세개의 아이콘이 있음
 class Desktop {
-	/* TODO: Desktop 클래스는 어떤 멤버함수와 멤버변수를 가져야 할까요? */
+    constructor(background, icon, folder) {  // 생성자
+        this.icon = new Icon();
+        this.folder = new Folder();
+    }
 };
 
+// 일반 아이콘
 class Icon {
-	/* TODO: Icon 클래스는 어떤 멤버함수와 멤버변수를 가져야 할까요? */
+    constructor(icon){
+        console.log("Icon 생성");
+        this.icon = icon;
+    }
+    click(){
+
+    }
 };
 
+// 폴더 아이콘
 class Folder {
-	/* TODO: Folder 클래스는 어떤 멤버함수와 멤버변수를 가져야 할까요? */
+    constructor(folder) {
+        console.log("Folder 생성");
+        this.folder = folder;
+    }
+    click(){
+
+    }
+    doubleClick(){
+        console.log("Click!!");
+        let window = new Window();
+    }
 };
 
+// 열린 폴더의 화면
 class Window {
-	/* TODO: Window 클래스는 어떤 멤버함수와 멤버변수를 가져야 할까요? */
+    constructor() {
+    }
 };
+
+let myDeskTop = new Desktop(123);
