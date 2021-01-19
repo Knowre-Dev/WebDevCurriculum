b2da691bc154e79b6b5790d8877f14d5ab2037e6
Song-YunMin <thd930308@naver.com>
2021-01-19 10:12:35 +0900

    [UPDATE] : Folder class 필요성?

--- Part 2. JavaScript/Quest 06. JavaScript OOP - Design/skeleton/desktop.js
+++ Part 2. JavaScript/Quest 06. JavaScript OOP - Design/skeleton/desktop.js
@@ -2,49 +2,48 @@
 class Desktop {
     constructor() {                     // 생성자
         this.icon = new Icon();         // 아이콘, 폴더생성
-        this.folder = new Folder();
+        // this.folder = new Folder();
     }
 
     // 클릭 된 요소 아이디 얻음
     getID(element){
-        const id = element.getAttribute('class');       // 클릭한 Element 의 class 값 가져옴
-        if(id === 'icon'){                                          // icon 인지 Folder 인지 판단함
-            this.icon.Drag(element);
-        }
-        else if(id === 'folder'){
-            this.folder.Drag(element);
-        }
+        this.icon.Drag(element);
+        // const id = element.getAttribute('class');       // 클릭한 Element 의 class 값
+        // if(id === 'icon'){                                          // icon 인지 Folder 인지 판단
+        //     this.icon.Drag(element);
+        // }
+        // else if(id === 'folder'){
+        //     this.folder.Drag(element);
+        // }
     }
 }
 
 // 일반 아이콘
 class Icon {
     constructor() {
-        // 아이콘 출력
         this.mEvent = new Mouse();
-        // TODO : 화면에 아이콘 뿌려주는 코드?
     }
     Drag(element){
         this.mEvent.mouseEvent(element);
     }
 }
 
-// 폴더 아이콘
-class Folder extends Icon{          // Open 기능만 추가하면 됨
-    constructor() {
-        // 폴더 출력
-        super();
-    }
-    Drag(element) {
-        super.Drag(element);
-    }
-}
+// // 폴더 아이콘
+// class Folder extends Icon{          // Open 기능만 추가하면 됨
+//     constructor() {
+//         super();
+//     }
+//     Drag(element) {
+//         super.Drag(element);
+//     }
+// }
 
 // 마우스 이벤트
 class Mouse{
     constructor(element) {                      // 요소 값
         this.element = element;
     }
+
     mouseEvent(element){
         element.onmouseup = function (){        // Mouse Up
             console.log("Mouse Up");
