27be1a788b6e786fab938ae44e558f71bd705ada
YunMin-Song <thd930308@naver.com>
2021-01-19 23:13:57 +0900

    [BUG] : Button 드래그 시 mouseup 제대로 동작하지 않음(div Elemenet는 동작)

--- Part 2. JavaScript/Quest 07. JavaScript OOP - Practice/skeleton/desktop.js
+++ Part 2. JavaScript/Quest 07. JavaScript OOP - Practice/skeleton/desktop.js
@@ -16,86 +16,67 @@ class Icon {
     constructor() {
         this.mEvent = new Mouse();
     }
-
-    Drag(element) {
-        this.mEvent.mouseEvent(element);
-    }
 }
 
 // 마우스 이벤트
 class Mouse {
-    // HTML 에서 받아오는게 아
+    // HTML 에서 받아오는게 아님
     constructor() {                      // 요소 값
-        this.a = '1';
-        this.drag = null;
-        this.originTop = null;
-        this.originLeft = null;
-        this.originX = null;
-        this.originY = null
         this.mouseEvent();
     }
 
-    determiningValue(value) {
-        return (value.classList.contains('f'));
-    }
-
-    getPositioningValue(node, e) {
-        this.originTop = node.offsetTop;
-        this.originLeft = node.offsetLeft;
-        this.originX = e.clientX;
-        this.originY = e.clientY;
-    }
-
-    setPositioningValue(node, e){
-        const diffX = e.clientX - this.originX;
-        const diffY = e.clientY - this.originY;
-        const desktopBox = document.querySelector('.desktop').getBoundingClientRect();
-        const smallBox = node.getBoundingClientRect();
-        console.log(desktopBox);
-        console.log(smallBox);
-        // Math.min(Math.max(최소위치, resultX), 최대위치)
-        const resultTop = Math.min(
-            Math.max(0, this.originTop + diffY),
-            desktopBox.height - smallBox.height
-        );
-        const resultLeft = Math.min(
-            Math.max(0, this.originLeft + diffX),
-            desktopBox.width - smallBox.width
-        );
-        console.log(resultTop+'px');
-        node.style.top = `${resultTop}+px`;
-        node.style.left = `${resultLeft}+px`;
-    }
-
     mouseEvent() {
-        const classNode = document.querySelectorAll('.iconImg');     // NodeList
-        console.log(classNode);
-
-        for (let node of classNode) {
-            node.addEventListener('mousedown', e => {
-                this.drag = true;
-                this.getPositioningValue(node, e);
+        let drag = false;
+        let originTop;
+        let originLeft;
+        let originX;
+        let originY;
+        const classNode = document.querySelectorAll('.icon');     // NodeList
+        const box = document.querySelector('.box');
+        // for (let node of classNode) {
+            box.addEventListener('mousedown', e => {
+                drag = true;
+                originTop = box.offsetTop;
+                originLeft = box.offsetLeft;
+                originX = e.clientX;
+                originY = e.clientY;
+                console.log("Mouse down");
             })
-
             document.addEventListener('mouseup', e => {
-                this.drag = false;
+                drag = false;
+                console.log("Mouse Up");
             })
-
             document.addEventListener('mousemove', e => {
-                if (this.drag) {
-                    console.log("move");
-                    this.setPositioningValue(node, e);
-                }
-            })
+                if (drag) {
+                    const diffX = e.clientX - originX;
+                    const diffY = e.clientY - originY;
+                    const desktopBox = document.querySelector('.desktop')
+                        .getBoundingClientRect();
+                    const smallBox = box.getBoundingClientRect();
 
-            node.addEventListener('dblclick', e => {
-                console.log("Mouse double Click");
-                if (this.determiningValue(node)) {
-                    this.window = new Window();
+                    // Math.min(Math.max(최소위치, resultX), 최대위치)
+                    const resultTop = Math.min(
+                        Math.max(0, originTop + diffY),
+                        desktopBox.height - smallBox.height
+                    );
+
+                    const resultLeft = Math.min(
+                        Math.max(0, originLeft + diffX),
+                        desktopBox.width - smallBox.width
+                    );
+                    box.style.top = `${resultTop}px`;
+                    box.style.left = `${resultLeft}px`;
                 }
-            })
+            });
+            //
+            // node.addEventListener('dblclick', e => {
+            //     console.log("Mouse double Click");
+            //     if (this.determiningValue(node)) {
+            //         this.window = new Window();
+            //     }
+            // })
         }
-    }
+    // }
 }
 
 // 열린 폴더의 화면
