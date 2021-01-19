bcab5cbdfb0e1f178e675fcda6b08ff5b0f6e35f
Song-YunMin <thd930308@naver.com>
2021-01-19 15:30:07 +0900

    [UPDATE] : Mouse Event 처리방식 변경

--- Part 2. JavaScript/Quest 07. JavaScript OOP - Practice/skeleton/desktop.js
+++ Part 2. JavaScript/Quest 07. JavaScript OOP - Practice/skeleton/desktop.js
@@ -1,15 +1,78 @@
+// 처음에는 세개의 아이콘이 있음
 class Desktop {
-	/* TODO: Desktop 클래스는 어떤 멤버함수와 멤버변수를 가져야 할까요? */
-};
+    constructor() {                     // 생성자
+        this.icon = new Icon();         // 아이콘, 폴더생성
+        // this.folder = new Folder();
+    }
 
+    // 클릭 된 요소 아이디 얻음
+    getID(element){
+        this.icon.Drag(element);
+    }
+}
+
+// 일반 아이콘
 class Icon {
-	/* TODO: Icon 클래스는 어떤 멤버함수와 멤버변수를 가져야 할까요? */
-};
+    constructor() {
+        this.mEvent = new Mouse();
+    }
+    Drag(element){
+        this.mEvent.mouseEvent(element);
+    }
+}
 
-class Folder {
-	/* TODO: Folder 클래스는 어떤 멤버함수와 멤버변수를 가져야 할까요? */
-};
+// 마우스 이벤트
+class Mouse{
+    constructor(element) {                      // 요소 값
+        this.element = element;
+        this.mouseEvent(element);
+    }
 
+    mouseEvent(element) {
+        const icon = document.querySelector('.bt');
+        let drag = false;
+        let originTop;
+        let originLeft;
+        let originX;
+        let originY;
+
+        icon.addEventListener('mouseup', e=>{
+            console.log("Mouse Up!");
+        });
+        // element.onmousedown = function(){
+        //     console.log("Mouse Down");
+        //     drag = true;
+        //
+        // }
+        icon.addEventListener('mousedown', e=>{
+            console.log("Mouse Down!");
+        })
+        // 더블 클릭을 했을때
+        icon.ondblclick = function(){        // Mouse Double Click
+            console.log("Mouse double Click");
+            const buf = element.getAttribute('class');
+            if(buf === 'folder') {
+                this.window = new Window(element);          // 새 창
+            }
+        }
+    }
+}
+
+// 열린 폴더의 화면
 class Window {
-	/* TODO: Window 클래스는 어떤 멤버함수와 멤버변수를 가져야 할까요? */
-};
+    constructor(element) {
+        this.element = element;
+        console.log("Windows Object");
+    }
+    openWindow() {
+
+    }
+    closeWindow() {
+
+    }
+}
+
+const myDeskTop = new Desktop();
+
+// TODO : Folder 지우는 것 고려
+// TODO : 10~ 16 Line 지우는 것 고려
