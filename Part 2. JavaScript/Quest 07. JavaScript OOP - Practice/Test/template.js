e2941155a6ed266f821e2a8e90aec3e0945ab9e7
YunMin-Song <thd930308@naver.com>
2021-01-22 03:17:55 +0900

    [ETC] : 작업공간 변경

--- Part 2. JavaScript/Quest 07. JavaScript OOP - Practice/Test/template.js
+++ Part 2. JavaScript/Quest 07. JavaScript OOP - Practice/Test/template.js
@@ -1,8 +1,7 @@
 class Desktop{
-    constructor(color, desktop) {
-        this.color = color;
+    constructor(icon, desktop) {
+        this.icon = icon;
         this.desktop = desktop;
-        this.useIt();
         this.supportsTemplate();
     }
 
@@ -10,26 +9,6 @@ class Desktop{
         return console.log('content' in document.createElement('template'));
     }
 
-    useIt(){
-        // const desktop = document.querySelector("#template-desktop");
-        // desktop.content.querySelector(".desktop");
-        // let clone = document.importNode(desktop.content, true);
-        // document.body.appendChild(clone);
-        //
-        // const desktopColor = document.querySelector(".desktop");
-        // // console.log(desktopColor);
-        // desktopColor.style.backgroundColor = this.color;
-
-        let content = document.querySelector('template').content;   // template 활성화
-        let span = content.querySelector('span');                   // template 내 span 요소 선택
-        console.log(span);
-        span.textContent = parseInt(span.textContent) + 1;                  // span 요소의 text를 1씩 증가
-        document.querySelector('#container').appendChild(           // container 요소에 template 요소를 append
-            document.importNode(content,true));
-
-
-        return this;
-    }
 }
 
 class Icon{
