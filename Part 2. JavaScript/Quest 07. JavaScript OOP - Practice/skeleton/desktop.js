da7d1b13723ee64a4687eceb1a876fc28b367ffe
Song-YunMin <thd930308@naver.com>
2021-01-22 17:19:18 +0900

    [UPDATE] : Window Class 정의

--- Part 2. JavaScript/Quest 07. JavaScript OOP - Practice/skeleton/desktop.js
+++ Part 2. JavaScript/Quest 07. JavaScript OOP - Practice/skeleton/desktop.js
@@ -105,13 +105,15 @@ class Folder {
     }
 
     addEvents() {
+        // Folder Icon 더블클릭 이벤트가 존재하므로 Option 값 포함
         new DraggableHandler(this.#dom, { dblClick: true });
     }
 }
 
+// Mouse Event Class
 class DraggableHandler {
-    #dom
-    #option
+    #dom            // Private Field
+    #option         // dblClick Option
 
     constructor(dom, option = {}) {
         this.#dom = dom;
@@ -153,8 +155,8 @@ class DraggableHandler {
     
             const mouseUpEvent = document.addEventListener('mouseup', () => {
                 pushed = false;
-                document.removeEventListener('mousemove', mouseMoveEvent);
-                document.removeEventListener('mouseup', mouseUpEvent);
+                // document.removeEventListener('mousemove', mouseMoveEvent);
+                // document.removeEventListener('mouseup', mouseUpEvent);
             });
         });
     }
@@ -162,83 +164,28 @@ class DraggableHandler {
     addDblClick() {
         this.#dom.addEventListener('dblclick', e => {
             console.info(e);
+            new Window(this.#dom);
         });
     }
 }
 
+class Window{
+    #dom
 
-// // Mouse Class
-// class Mouse {
-//     constructor() {
-//         this.mouseEvent();
-//     }
+    constructor(dom) {
+        this.#dom = dom;
+        this.prepareDom();          // Ready to Template
+        this.createWindow();
+    }
 
-//     mouseEvent() {
-//         window.addEventListener("load", function () {
-//             const desktop = document.querySelector(".desktop");
-//             let dragging = false;
-//             let offset = {x: 0, y: 0};
-//             let current = null;
-//             let left = desktop.offsetLeft;
-//             let top = desktop.offsetTop;
+    prepareDom(){
+        const t = document.querySelector('.template-window');
+        const tmpl = document.importNode(t.content, true);
+        this.#dom = tmpl.querySelector('.window');
+    }
 
-//             desktop.onmousedown = function (e) {
-//                 if (e.target.classList.contains("icon") ||
-//                     e.target.classList.contains("window")) {
-//                     dragging = true;
-//                     current = e.target;
-//                     offset.x = e.offsetX;
-//                     offset.y = e.offsetY;
-//                 }
-//             };
-
-//             desktop.onmousemove = function (e) {
-//                 if (!dragging) return;
-//                 current.style.left = e.pageX - offset.x - left + "px";
-//                 current.style.top = e.pageY - offset.y - top + "px";
-//             };
-
-//             desktop.onmouseup = function (e) {
-//                 dragging = false;
-//             };
-
-//             desktop.ondblclick = function (e) {
-//                 if (e.target.classList.contains("f")) {  // folder 일때만
-//                     const window = new Window();
-//                     window.openWindow(e.target);
-//                 }
-//             };
-//         })
-//     }
-// }
-
-// // 열린 폴더의 화면
-// class Window {
-//     constructor() {                         // element : e.target  (Folder 만 들어옴)
-//         console.log("Window Class");
-//         this.mEvent = new Mouse();          // Mouse 객체 생성
-//     }
-
-//     openWindow(element) {                  // New Window Open
-//         console.log(element.getAttribute('class'));
-//         const window = document.querySelector(".window");
-//         const name = element.getAttribute("id");
-//         window.style.visibility = "visible";            // Folder 숨겨 둠 (이게 맞냐..?)
-//         window.innerHTML =                              // Insert HTML
-//             "<span class=\'title\'></span>" +
-//             "<div id=\'xbox\' class=\'xbox\'>" +
-//             "<span class=\'x\'>X</span>" +
-//             "</div>";
-
-//         const xbox = document.querySelector(".desktop .window .xbox");
-//         const title = document.querySelector(".desktop .window .title");
-//         title.innerHTML = name;
-//         this.closeWindow(window, xbox);
-//     }
-
-//     closeWindow(window, xbox) {
-//         xbox.onclick = function (e) {
-//             window.style.visibility = "hidden";
-//         }
-//     }
-// }
+    createWindow(){
+        console.log("Ready to Windows!!");
+        console.log(this.#dom);
+    }
+}
