3db183b2fc8854e9d7b681f50a8c329d2ee359b7
Song-YunMin <thd930308@naver.com>
2021-01-27 17:53:02 +0900

    [ETC] 작업공간 변경

---
+++ Part 2. JavaScript/Quest 08. Midterm project/To-Do-App/Clock.js
@@ -0,0 +1,21 @@
+class Clock{
+    #dom
+    constructor(dom) {
+        this.#dom = dom;
+        this.prepareDom();
+    }
+
+    prepareDom(){
+        const t = document.querySelector('.template-clock');
+        const tmpl = document.importNode(t.content, true);
+        this.#dom = tmpl.querySelector('.clock');
+    }
+
+    getDom(){
+        return this.#dom;
+    }
+
+    createClock(){
+
+    }
+}
--- Part 2. JavaScript/Quest 08. Midterm project/To-Do-App/Desktop.js
+++ Part 2. JavaScript/Quest 08. Midterm project/To-Do-App/Desktop.js
@@ -1,10 +1,16 @@
 // 배경화면 관련 기능 구현
 class Desktop {
     #dom
+    #clock
     constructor(dom) {
         this.#dom = dom;
+
+        this.#clock = new Clock();
+        this.showImage(this.#clock);
     }
 
-    
+    showImage(dom){
+
+    }
 
 }
